d04e38af3f0d2184f355848b3615c435006c98c3
Saurabh Sharma <saurabh.ss668@gmail.com>
2026-08-26 21:53:57 +0530

    Git CI/CD Pipeline Fixes

--- resources/views/rss.blade.php
+++ resources/views/rss.blade.php
@@ -2,7 +2,7 @@
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 <channel>
     <title>{{ \App\Models\Setting::get('site_name', 'My Portfolio') }} Blog</title>
-    <link>{{ env('FRONTEND_URL', 'http://localhost:3000') }}</link>
+    <link>{{ config('app.frontend_url', 'http://localhost:3000') }}</link>
     <description>{{ \App\Models\Setting::get('site_meta_description', 'Latest blog posts and developer notes') }}</description>
     <language>en-us</language>
     <lastBuildDate>{{ now()->toRssString() }}</lastBuildDate>
@@ -10,10 +10,10 @@
     @foreach($posts as $post)
         <item>
             <title><![CDATA[{{ $post->title }}]]></title>
-            <link>{{ env('FRONTEND_URL', 'http://localhost:3000') }}/blogs/{{ $post->slug }}</link>
+            <link>{{ config('app.frontend_url', 'http://localhost:3000') }}/blogs/{{ $post->slug }}</link>
             <description><![CDATA[{{ $post->excerpt }}]]></description>
             <pubDate>{{ $post->published_at ? $post->published_at->toRssString() : $post->created_at->toRssString() }}</pubDate>
-            <guid isPermaLink="true">{{ env('FRONTEND_URL', 'http://localhost:3000') }}/blogs/{{ $post->slug }}</guid>
+            <guid isPermaLink="true">{{ config('app.frontend_url', 'http://localhost:3000') }}/blogs/{{ $post->slug }}</guid>
             @if($post->user)
                 <author>{{ $post->user->email }} ({{ $post->user->name }})</author>
             @endif
